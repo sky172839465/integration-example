dbb648b52cb6dc1346b2e2c17a734bc169b925a2
Yusong Hsu <sky172839465@gmail.com>
2021-04-19 00:19:10 +0800

    feat: add saucectl

--- __e2e__/App2.test.js
+++
@@ -1,9 +0,0 @@
-describe('Localhost', () => {
-  beforeAll(async () => {
-    await page.goto('http://localhost:3000')
-  })
-
-  it('should be titled "React App"', async () => {
-    await expect(page.title()).resolves.toMatch('React App')
-  })
-})
