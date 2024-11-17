5702ab1c99a975e0687793a9d220ab2bc3273a3f
sky172839465 <sky172839465@gmail.com>
2024-11-17 15:01:17 +0800

    update

--- __tests__/App2.test.js
+++ __tests__/App2.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 
-import App from '../src/App'
+import App from '../src/App.jsx'
 
 test('renders learn react link 2', () => {
   render(<App />)
